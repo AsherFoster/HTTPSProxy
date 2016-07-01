d6990f84404c3be79ff163cc3dac42f3ce962f1c
Asher Foster <asher@developster.io>
2016-07-02 11:26:22 +1200

    Debug

--- main.js
+++ main.js
@@ -17,20 +17,20 @@ app.get('/api', function(req, res){
     var url = req.query.url,
         host = req.headers.host.split(base).join();
     console.log(host);
-    if(url){
-        var protocol = url.split(':')[0];
-        if(protocol === 'http' || protocol === 'https'){
-            var body = "";
-            (protocol === 'https' ? https : http).get(url, function(resp){
-                resp.on('data', function(chunk) {
-                    body += chunk;
-                });
-                resp.on('end', function() {
-                    res.send(body);
-                });
-            });
-        }else res.send(400).send("Must be a HTTP or HTTPS URL");
-    }else res.status(400).send("Must have URL query field");
+    // if(url){
+    //     var protocol = url.split(':')[0];
+    //     if(protocol === 'http' || protocol === 'https'){
+    //         var body = "";
+    //         (protocol === 'https' ? https : http).get(url, function(resp){
+    //             resp.on('data', function(chunk) {
+    //                 body += chunk;
+    //             });
+    //             resp.on('end', function() {
+    //                 res.send(body);
+    //             });
+    //         });
+    //     }else res.send(400).send("Must be a HTTP or HTTPS URL");
+    // }else res.status(400).send("Must have URL query field");
 });
 app.listen(port);
 console.log("Ready on port "+port);
